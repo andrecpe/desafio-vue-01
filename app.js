b3fe79ed800ebf555ef0f47492e7b70c2f8ba595
andrecpe <andrecpe@gmail.com>
2020-11-10 21:09:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'André',
+        idade: 4,
+        imagem: 'http://4.bp.blogspot.com/-SKPA4ogtPlc/VwLBCd_aGkI/AAAAAAABNow/M-Q7oL16e4UAWzZm1cHlQplpvmlMWhTDg/s1600/cac63409.gif'
+    },
+    methods: {
+        tres: () => this.idade * 3,
+        numero: () => Math.random()
+    }
+})
